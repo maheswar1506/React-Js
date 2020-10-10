83bbd2dd695dfde5f5ef882eb8c2612ca490e47a
maheswar1506 <maheswarreddy1506@gmail.com>
2020-10-10 21:11:46 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Row, Col, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -87,24 +88,24 @@ class CommentForm extends Component {
 }
 
 function RenderDish({dish}) {
-  if(dish != null) {
-    return(
-    	<div className="col-12 col-md-5 m-1">
-        	<Card>
-        	  	<CardImg width="100%" src={dish.image} alt={dish.name} />
-        	  	<CardBody>
-            		<CardTitle>{dish.name}</CardTitle>
-            		<CardText>{dish.description}</CardText>
-          		</CardBody>
-        	</Card>
-        </div>
-      );
-  }
-  else {
-    return(
-        <div></div>
-      );
-  }
+	if(dish != null) {
+	return(
+		<div className="col-12 col-md-5 m-1">
+	    	<Card>
+	    	  	<CardImg width="100%" src={dish.image} alt={dish.name} />
+	    	  	<CardBody>
+	        		<CardTitle>{dish.name}</CardTitle>
+	        		<CardText>{dish.description}</CardText>
+	      		</CardBody>
+	    	</Card>
+	    </div>
+	  );
+	}
+	else {
+	return(
+	    <div></div>
+	  );
+	}
 }
 
 function RenderComments({comments, addComment, dishId}) {
@@ -132,7 +133,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-	if(props.dish != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+	else if(props.dish != null) {
 		return(
 			<div className="container">
                 <div className="row">
